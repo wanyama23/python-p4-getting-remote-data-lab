e17f43427e1f4b141aa5eac5245876da836d5613
edmond <edmond.wanyama@student.moringaschool.com>
2023-09-23 03:18:29 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.content
+        
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        data = json.loads(response_body)
+        return data
+    
